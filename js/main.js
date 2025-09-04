f928f0f134deb9512bb2e482da4e3cae837724e0
Óscar Javier González Manríquez <128267797+Pinoberry@users.noreply.github.com>
2025-09-03 23:08:10 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 
-const selectorColor = document.getElementById('selectorColor'); // El input donde el usuario elige el color
-const muestraColor = document.querySelector('.muestra-color'); // El div que mostrará el color
-const codigoHex = document.getElementById('codigoHex'); // El párrafo donde se muestra el código hexadecimal
+const selectorColor = document.getElementById('selectorColor'); 
+const muestraColor = document.querySelector('.muestra-color'); 
+const codigoHex = document.getElementById('codigoHex'); 
 
 
 selectorColor.addEventListener('input', (evento) => {
